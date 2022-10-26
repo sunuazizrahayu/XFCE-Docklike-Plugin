7f1c12e86ddba56f1aa70c1e899ae6c447bcb6ca
sunuazizrahayu <sunuazizrahayu@gmail.com>
2022-10-26 15:15:35 +0700

    Update xfce4-docklike-plugin.sh
    add for vanilla debian 11

--- xfce4-docklike-plugin.sh
+++ xfce4-docklike-plugin.sh
@@ -3,7 +3,8 @@
 #Version 1.0
 cd ~/Downloads
 sudo apt update && sudo apt upgrade
-sudo apt install wget xorg-dev libglib2.0-cil-dev golang-gir-gio-2.0-dev libgtk-3-dev libwnck-3-dev libxfce4ui-2-dev libxfce4panel-2.0-dev intltool
+sudo apt install wget xorg-dev libglib2.0-cil-dev golang-gir-gio-2.0-dev libgtk-3-dev libwnck-3-dev libxfce4ui-2-dev libxfce4panel-2.0-dev intltool \
+  make gcc g++
 wget https://archive.xfce.org/src/panel-plugins/xfce4-docklike-plugin/0.4/xfce4-docklike-plugin-0.4.0.tar.bz2
 tar -xvjf xfce4-docklike-plugin-0.4.0.tar.bz2 && cd xfce4-docklike-plugin-0.4.0
 ./configure
